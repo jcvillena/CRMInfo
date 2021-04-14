8a4c83aed5f6ea805db607325996dd03ebabaa28
Juan Carlos Villena <dehitex@hotmail.com>
2021-04-14 12:32:47 +0200

    especiales

---
+++ Data/Especial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMInfo.Data
+{
+    public class Especial
+    {
+
+        public string Oferta { get; set; }
+        public DateTime Fecha { get; set; }
+        public string SuPedido { get; set; }
+        public string CreadoPor { get; set; }
+        public string Codigo { get; set; }
+        public string Estado { get; set; }
+
+        
+        public double Total { get; set; }
+
+    }
+}
---
+++ Data/EspecialService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMInfo.Data
+{
+    public class EspecialService
+    {
+        private JDEDataAccessLayer _dataLayer;
+        public EspecialService(JDEDataAccessLayer dataLayer)
+        {
+            _dataLayer = dataLayer;
+        }
+
+        public async Task<List<Especial>> GetEspeciales(double cliente)
+        {
+            List<Especial> lista = await _dataLayer.GetEspeciales(cliente);
+
+
+            return lista; // Task.FromResult(lista);
+        }
+    }
+}
